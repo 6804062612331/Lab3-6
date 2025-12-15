d25ab10222d59835924f8e3b02c94b3688e7d88d
6804062612331 <s6804062612331@email.kmutnb.ac.th>
2025-12-15 10:02:41 +0700

    Add files via upload

---
+++ grading.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(){
+	int score;
+	scanf("%d",&score);
+	if(score<100){
+		if(score>0){
+			if (score>68){
+		if(score>=80){
+			printf("A");
+		}
+		else if(score>=75){
+			printf("B");
+		}
+		else if(score>=68){
+			printf("C %d",75-score);
+		}	
+	}
+	else if(score>=55){
+			printf("D");
+			}
+	else{
+		printf("F");
+	}	
+			
+		}
+		else {
+		printf("Score Error");	
+		}
+	}
+	else {
+		printf("Score Error");
+	}
+}
